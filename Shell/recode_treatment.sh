92333f3c123c182d03550d6ef3d4c097d5c9c34b
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2016-11-17 09:22:25 +1100

    fixed spelling

--- Shell/recode_treatment.sh
+++ Shell/recode_treatment.sh
@@ -3,6 +3,6 @@
 
 sed -i '' 's/Drought/Reduced/g' *.R
 sed -i '' 's/Pulsed.drought/Reduced.frequency/g' *.R
-sed -i '' 's/Seasonl/Summer.drought/g' *.R
+sed -i '' 's/Seasonal/Summer.drought/g' *.R
 
 
